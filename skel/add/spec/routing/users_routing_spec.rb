1c694f2260b5ce596f0beb94a977455bd675c941
Clifford T. Matthews <ctm@devctm.com>
2009-10-12 12:30:20 -0600

    Switched the routes specs over to the new (with RSpec-rails 1.2.9) route_to method.  Finishes Support #96.

--- skel/add/spec/routing/users_routing_spec.rb
+++ skel/add/spec/routing/users_routing_spec.rb
@@ -1,47 +1,25 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
-describe Admin::UsersController do
-  describe "route generation" do
-    it "maps #new" do
-      route_for(:controller => "users", :action => "new").should == "/user/new"
+describe UsersController do
+  describe "routing" do
+    it "recognizes and generates #new" do
+      { :get => "/user/new" }.should route_to(:controller => "users", :action => "new")
     end
 
-    it "maps #show" do
-      route_for(:controller => "users", :action => "show").should == "/user"
+    it "recognizes and generates #show" do
+      { :get => "/user" }.should route_to(:controller => "users", :action => "show")
     end
 
-    it "maps #edit" do
-      route_for(:controller => "users", :action => "edit").should == "/user/edit"
+    it "recognizes and generates #edit" do
+      { :get => "/user/edit" }.should route_to(:controller => "users", :action => "edit")
     end
 
-    it "maps #create" do
-      route_for(:controller => "users", :action => "create").should == {:path => "/user", :method => :post}
+    it "recognizes and generates #create" do
+      { :post => "/user" }.should route_to(:controller => "users", :action => "create")
     end
 
-    it "maps #update" do
-      route_for(:controller => "users", :action => "update").should == {:path =>"/user", :method => :put}
-    end
-  end
-
-  describe "route recognition" do
-    it "generates params for #new" do
-      params_from(:get, "/user/new").should == {:controller => "users", :action => "new"}
-    end
-
-    it "generates params for #create" do
-      params_from(:post, "/user").should == {:controller => "users", :action => "create"}
-    end
-
-    it "generates params for #show" do
-      params_from(:get, "/user").should == {:controller => "users", :action => "show"}
-    end
-
-    it "generates params for #edit" do
-      params_from(:get, "/user/edit").should == {:controller => "users", :action => "edit"}
-    end
-
-    it "generates params for #update" do
-      params_from(:put, "/user").should == {:controller => "users", :action => "update"}
+    it "recognizes and generates #update" do
+      { :put => "/user" }.should route_to(:controller => "users", :action => "update")
     end
   end
 end
